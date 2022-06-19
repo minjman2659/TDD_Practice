1a5cbde4aeed4927292e7a37c75f4edd0144c8ff
minjman2659 <minjman2659@gmail.com>
2022-06-19 18:08:02 +0900

    Test: [Mock] 목 테스트 코드 작성

--- src/mock/check/test/check.spec.js
+++ src/mock/check/test/check.spec.js
@@ -1,15 +1,27 @@
 const check = require('../check');
 
 describe('check', () => {
-  const mockPredicateTrue = jest.fn(() => true);
-  const mockPredicateFalse = jest.fn(() => false);
-  const mockOnSuccess = jest.fn((el) => el);
-  const mockOnFail = jest.fn((el) => el);
+  let onSuccess;
+  let onFail;
 
-  it('Yes', () => {
-    expect(check(mockPredicateTrue, mockOnSuccess, mockOnFail)).toBe('yes');
+  beforeEach(() => {
+    onSuccess = jest.fn();
+    onFail = jest.fn();
   });
-  it('No', () => {
-    expect(check(mockPredicateFalse, mockOnSuccess, mockOnFail)).toBe('no');
+
+  it('should call onSuccess when predicate is true', () => {
+    check(() => true, onSuccess, onFail);
+    //* onSuccess 목함수가 최소 한번은 호출 되었는지 여부 테스트
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    //* onSuccess 목함수에 첫번째로 호출된 첫번째 인자는 'yes'이다 여부 테스트
+    expect(onSuccess).toHaveBeenCalledWith('yes');
+    //* onFail 목함수는 호출되지 말아야 한다.
+    expect(onFail).toHaveBeenCalledTimes(0);
+  });
+  it('should call onFail when predicate is false', () => {
+    check(() => false, onSuccess, onFail);
+    expect(onFail).toHaveBeenCalledTimes(1);
+    expect(onFail).toHaveBeenCalledWith('no');
+    expect(onSuccess).toHaveBeenCalledTimes(0);
   });
 });
